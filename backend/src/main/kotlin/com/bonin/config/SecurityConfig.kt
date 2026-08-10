c56e577c4896e6de783103b5e826cb0344271c2a
Farah Fairuz <farahfairuz014@gmail.com>
2026-08-10 23:08:33 +0700

    chore(backend): setup health & db

---
+++ backend/src/main/kotlin/com/bonin/config/SecurityConfig.kt
@@ -0,0 +1,42 @@
+﻿package com.bonin.config
+
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    fun actuatorFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            securityMatcher(EndpointRequest.toAnyEndpoint())
+            csrf { disable() }
+            authorizeHttpRequests {
+                authorize(anyRequest, permitAll)
+            }
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    fun appFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf { disable() }
+            formLogin { disable() }
+            httpBasic { disable() }
+            authorizeHttpRequests {
+                authorize(anyRequest, denyAll)
+            }
+        }
+        return http.build()
+    }
+}
